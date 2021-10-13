6fc61036142e8d58aaea1556d8c259b644b1e59e
Alex Thalhammer <alex@angular-software.at>
2021-10-13 13:20:16 +0200

    [Routing] Lazy loading

--- src/app/flight-booking/flight-search/flight-search.component.ts
+++ src/app/flight-booking/flight-search/flight-search.component.ts
@@ -83,6 +83,6 @@ export class FlightSearchComponent implements OnDestroy {
   }
 
   onEdit(id: number): void {
-    this.router.navigate(['/flight-edit', id, { showDetails: true }]);
+    this.router.navigate(['/flight-booking', 'flight-edit', id, { showDetails: true }]);
   }
 }
